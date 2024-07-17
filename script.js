7506f6fa7cfc7ed751cf8763d5d5937e1457aa8a
Avaz Rakhimov <134701615+AvazRakhimov@users.noreply.github.com>
2024-07-17 20:42:32 +0500

    Major Update V2

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function calc() {
-    let n1 = parseFloat(document.getElementById("1input").value);
-    let n2 = parseFloat(document.getElementById("2input").value);
-    let math = document.getElementById("math").value;
+  let n1 = parseFloat(document.getElementById('1input').value);
+  let n2 = parseFloat(document.getElementById('2input').value);
+  let math = document.getElementById('math').value;
 
-    let result;
-    switch (math) {
-        case ("+"):
-            result = n1 + n2;
-            break;
-        case ("-"):
-            result = n1 - n2;
-            break;
-        case ("*"):
-            result = n1 * n2;
-            break;
-        case ("/"):
-            result = n1 / n2;
-            break;
-        case ("%"):
-            result = n1 % n2;
-            break;
-    }
+  let result;
+  switch (math) {
+    case '+':
+      result = n1 + n2;
+      break;
+    case '-':
+      result = n1 - n2;
+      break;
+    case '*':
+      result = n1 * n2;
+      break;
+    case '/':
+      result = n1 / n2;
+      break;
+    case '%':
+      result = n1 % n2;
+      break;
+  }
 
-    document.getElementById("result").innerHTML = "Result: " + result;
+  document.getElementById('result').innerHTML = 'Result: ' + result;
 }
