988b81809dd8496a0d97658f851d17be26c65bde
Avaz Rakhimov <134701615+AvazRakhimov@users.noreply.github.com>
2024-07-28 01:06:39 +0500

    Updates
    Added `copy` result feature
    Removed `footer`
    Fixes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Calculation //
 function calc() {
   let n1 = parseFloat(document.getElementById('1input').value)
   let n2 = parseFloat(document.getElementById('2input').value)
@@ -24,3 +25,16 @@ function calc() {
 
   document.getElementById('result').innerHTML = 'Result: ' + result
 }
+
+// Copy //
+document.getElementById('result').addEventListener('click', function () {
+  const resultText = document.getElementById('result').innerText
+  navigator.clipboard
+    .writeText(resultText)
+    .then(function () {
+      alert('Result copied to clipboard!')
+    })
+    .catch(function (err) {
+      console.error('Failed to copy text: ', err)
+    })
+})
