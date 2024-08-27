da25c9917511413259a32a5b7c4b63f9cd04fd12
Avaz Rakhimov <134701615+AvazRakhimov@users.noreply.github.com>
2024-08-27 16:08:55 +0300

    Made more readable

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
-const input1=document.getElementById("1input"),input2=document.getElementById("2input"),resultDisplay=document.getElementById("result");let selectedOperator=null,selectedButton=null;function selectOperator(e,t){selectedButton&&(selectedButton.disabled=!1),selectedOperator=e,selectedButton=t,selectedButton.disabled=!0}function calc(){const e=parseFloat(input1.value),t=parseFloat(input2.value);let n;if(isNaN(e)||isNaN(t)||null===selectedOperator)resultDisplay.textContent="Please enter valid numbers and select an operator.";else{switch(selectedOperator){case"+":n=e+t;break;case"-":n=e-t;break;case"*":n=e*t;break;case"/":if(0===t)return void(resultDisplay.textContent="Cannot divide by zero.");n=e/t;break;case"%":n=e%t;break;default:return void(resultDisplay.textContent="Invalid operation.")}resultDisplay.textContent=`Result: ${n}`}}document.querySelectorAll(".sel-main button").forEach((e=>{e.addEventListener("click",(()=>selectOperator(e.textContent,e)))})),document.getElementById("result").addEventListener("click",(function(){const e=document.getElementById("result").innerText.replace("Result: ","").trim();navigator.clipboard.writeText(e).then((function(){alert("Copied to clipboard!")})).catch((function(e){console.error("Failed to copy text: ",e)}))}));
+// Calculation
+const input1 = document.getElementById('1input')
+const input2 = document.getElementById('2input')
+const resultDisplay = document.getElementById('result')
+let selectedOperator = null
+let selectedButton = null
+
+function selectOperator(operator, button) {
+  if (selectedButton) { selectedButton.disabled = false }
+  selectedOperator = operator
+  selectedButton = button
+  selectedButton.disabled = true
+}
+
+document.querySelectorAll('.sel-main button').forEach((button) => {
+  button.addEventListener('click', () => selectOperator(button.textContent, button))
+})
+
+function calc() {
+  const num1 = parseFloat(input1.value)
+  const num2 = parseFloat(input2.value)
+  let result
+
+  if (isNaN(num1) || isNaN(num2) || selectedOperator === null) {
+    resultDisplay.textContent = 'Please enter valid numbers and select an operator.'
+    return
+  }
+
+  switch (selectedOperator) {
+    case '+':
+      result = num1 + num2
+      break
+    case '-':
+      result = num1 - num2
+      break
+    case '*':
+      result = num1 * num2
+      break
+    case '/':
+      if (num2 !== 0) { result = num1 / num2 }
+      else {
+        resultDisplay.textContent = 'Cannot divide by zero.'
+        return
+      }
+      break
+    case '%':
+      result = num1 % num2
+      break
+    default:
+      resultDisplay.textContent = 'Invalid operation.'
+      return
+  }
+
+  resultDisplay.textContent = `Result: ${result}`
+}
+
+
+// Copy
+document.getElementById('result').addEventListener('click', function () {
+  const resultText = document.getElementById('result').innerText
+  const valueOnly = resultText.replace('Result: ', '').trim()
+  navigator.clipboard
+    .writeText(valueOnly)
+    .then(function () {
+      alert('Copied to clipboard!')
+    })
+    .catch(function (err) {
+      console.error('Failed to copy text: ', err)
+    })
+})
